0a9d4876c78a5b05b1685c7559b9b7ebb53c6e1a
Erik <eri.torres@duocuc.cl>
2023-04-21 16:06:49 -0400

    Primer commit

---
+++ src/Componentes/Carro.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Badge, Button, Popover, PopoverBody, PopoverHeader, Table } from "reactstrap";
+import listaCar from "../listaCarrito.json";
+
+class Carro extends React.Component{
+    constructor(){
+        const {listaCarrito} = listaCar;
+        super();
+        this.state={
+            popoverOpen: false,
+            listaCarrito
+        };
+
+        this.toggle=this.toggle.bind(this);
+    }
+
+    toggle(){
+        this.setState(prevState=> ({
+            popoverOpen: !prevState.popoverOpen
+        }));
+    }
+
+    render(){
+        const arregloCarrito = this.state.listaCarrito.map(
+            (listaCarrito, i) =>{
+                return(
+                    <tr key={i}>
+                        <td>{(i+=1)}</td>
+                        <td>{listaCarrito.titulo}</td>
+                        <td>{listaCarrito.precio}</td>
+                    </tr>
+                );
+            }
+        )
+
+        return(
+            <div>
+                <Button id="Popover1" color="info">
+                   <span className="material-icons">shopping_cart</span>
+                   <Badge color="success">{arregloCarrito.length}</Badge> 
+                </Button>
+                <Popover target="Popover1" placement="bottom" isOpen={this.state.popoverOpen} toggle={this.toggle}>
+                    <PopoverHeader>Listado de compras</PopoverHeader>
+                    <PopoverBody>
+                       <Table>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Producto</th>
+                                <th>Precio</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {arregloCarrito}
+                        </tbody>
+                        </Table> 
+                    </PopoverBody>
+                </Popover>
+            </div>
+        );
+    }
+}
+
+export default Carro;
